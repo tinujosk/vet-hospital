0523cda5dab292f0ca7ab031c6e20b0dc8aade90
Tinu Jos K <k.tinujos@gmail.com>
2024-10-06 23:13:08 -0400

    added jwt validity check and logout

--- client/src/util.js
+++ client/src/util.js
@@ -4,3 +4,15 @@ export const getUserDetailsFromToken = () => {
   const token = localStorage.getItem('token');
   if (token) return jwtDecode(token);
 };
+
+export const isTokenExpired = () => {
+  const decodedToken = getUserDetailsFromToken();
+  if (decodedToken) {
+    const currentTime = Date.now() / 1000;
+    console.log({ currentTime }, { decodedTokenTime: decodedToken.exp });
+    if (decodedToken.exp < currentTime) {
+      return true;
+    }
+  }
+  return false;
+};
