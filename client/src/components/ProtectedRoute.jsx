9f21445dec4b81fe00f510636c80ead0779a87d5
Tinu Jos K <k.tinujos@gmail.com>
2024-10-06 23:13:41 -0400

    ProtectedRoute cleanup and jwt expiry check

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useNavigate, Navigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import Layout from './Layout';
+import { isTokenExpired } from '../util';
+import { clearUserData } from '../slices/authSlice';
 
 // Role Based Access Control(RBAC)
 const hasPermission = (path, role) => {
@@ -19,52 +21,18 @@ const hasPermission = (path, role) => {
   return false;
 };
 
-const checkJWTValidity = () => {};
-// const dispatch = useDispatch();
-// const location = useLocation();
-// const navigate = useNavigate();
-
-// useEffect(() => {
-//   const token = localStorage.getItem('token');
-//   if (token) {
-//     try {
-//       const decodedToken = jwtDecode(token);
-//       const currentTime = Date.now() / 1000;
-
-//       if (decodedToken.exp < currentTime) {
-//         // Token is expired, log out
-//         dispatch(clearUserData());
-//         localStorage.removeItem('token');
-//         // navigate('/login');
-//       } else {
-//         // Token is valid, set user data in Redux
-//         dispatch(
-//           setUserData({
-//             userId: decodedToken.userId,
-//             email: decodedToken.email,
-//             role: decodedToken.role,
-//           })
-//         );
-//       }
-//     } catch (error) {
-//       // Invalid token
-//       dispatch(clearUserData());
-//       localStorage.removeItem('token');
-//       // navigate('/login');
-//     }
-//   }
-// }, [dispatch]);
-
-// const { role } = useSelector(state => state.auth);
-// const { role } = getUserDetailsFromToken() || {};
-// if (!role & (location.pathname !== '/login')) {
-//   return <div>Loading..</div>;
-// }
-
 const ProtectedRoute = ({ element, path, title }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const role = useSelector(state => state.auth.role);
   if (!role) {
     return <div>Page cannot be accessed or is Loading...</div>;
+  }
+  if (isTokenExpired()) {
+    // Token is expired, log out
+    dispatch(clearUserData());
+    localStorage.removeItem('token');
+    navigate('/');
   }
   return hasPermission(path, role) ? (
     <Layout title={title}>{element}</Layout>
